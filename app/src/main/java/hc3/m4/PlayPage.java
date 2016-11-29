6bca91b5f239892187cc241a62a821b622183160
luukhang <khang.luu@hotmail.com>
2016-11-28 20:40:38 -0500

    Basic swipe detection for play page

--- app/src/main/java/hc3/m4/PlayPage.java
+++ app/src/main/java/hc3/m4/PlayPage.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.MenuItemHoverListener;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 public class PlayPage extends AppCompatActivity {
 
@@ -42,4 +44,39 @@ public class PlayPage extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    // For detecting swipe
+    private float x1, x2;
+    static final int MIN_DISTANCE = 150;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+
+            // On touch, records x position
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+
+            // On finger release, record how far finger moved and calculate swipe
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+
+                // Swipe right
+                if (deltaX > MIN_DISTANCE) {
+                    Toast.makeText(this, "Swipe RIGHT", Toast.LENGTH_SHORT).show();
+                }
+
+                // Swipe left
+                else if (deltaX < MIN_DISTANCE * -1) {
+                    Toast.makeText(this, "Swipe LEFT", Toast.LENGTH_SHORT).show();
+                }
+
+                // Tap
+                else {
+                    Toast.makeText(this, "TAP", Toast.LENGTH_SHORT).show();
+                }
+        }
+        return super.onTouchEvent(event);
+    }
 }
